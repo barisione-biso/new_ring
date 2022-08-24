58ba0e67375bae238826d631f4da70c95469551e
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2022-08-24 10:42:31 -0400

    pring_gao scripts

---
+++ run_automated_test_wikidata_filtered_print_gao.sh
@@ -0,0 +1,2 @@
+./build/query-index ../data/wikidata-filtered-enumerated.dat.ring Queries/Queries-wikidata-benchmark.txt sigmod21 1 0 > experiment_output/sigmod21_gao.txt
+./build/query-index ../data/wikidata-filtered-enumerated.dat.ring Queries/Queries-wikidata-benchmark.txt one_ring_muthu_leap 1 0 > experiment_output/one_ring_muthu_leap_gao.txt
---
+++ run_automated_test_wikidata_full_print_gao.sh
@@ -0,0 +1,2 @@
+./build/query-index ../data/wikidata.nt.enumerated.ring Queries/Queries-bgps-limit1000.txt sigmod21 1 0 > experiment_output/sigmod21_gao.txt
+./build/query-index ../data/wikidata.nt.enumerated.ring Queries/Queries-bgps-limit1000.txt one_ring_muthu_leap 1 0 > experiment_output/one_ring_muthu_leap_gao.txt
