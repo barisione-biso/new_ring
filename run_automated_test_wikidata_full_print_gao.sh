5c0d96df419378a3381b84ed248f0b573a3571ef
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2022-08-26 18:14:02 -0400

    full test print gao

--- run_automated_test_wikidata_full_print_gao.sh
+++ run_automated_test_wikidata_full_print_gao.sh
@@ -1,2 +1,3 @@
 ./build/query-index ../data/wikidata.nt.enumerated.ring Queries/Queries-bgps-limit1000.txt sigmod21 1 0 > experiment_output/wikidata_full_sigmod21_gao.txt
 ./build/query-index ../data/wikidata.nt.enumerated.ring Queries/Queries-bgps-limit1000.txt one_ring_muthu_leap 1 0 > experiment_output/wikidata_full_one_ring_muthu_leap_gao.txt
+
