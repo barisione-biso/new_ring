a3a09ca862021e911b380590c9e1dacbef14ad92
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2022-10-02 14:10:58 -0300

    Backward search improvements

--- include/ltj_algorithm_spo_sop.hpp
+++ include/ltj_algorithm_spo_sop.hpp
@@ -436,7 +436,7 @@ namespace ring {
             std::vector<ltj_iter_type*>& itrs = m_var_to_iterators[x_j];
             
             if(!is_intersection_calculated(x_j)){
-                std::cout << "Intersecting ";
+                //std::cout << "Intersecting ";
                 std::vector<wm_type*> wms;
                 std::vector<sdsl::range_vec_type> ranges;
                 for(ltj_iter_type* iter : itrs){
@@ -446,9 +446,9 @@ namespace ring {
                     wms.emplace_back(&current_wm);
                     assert (cur_interval.right() >= cur_interval.left() );
                     ranges.emplace_back(sdsl::range_vec_type{{cur_interval.left(), cur_interval.right()}});
-                    std::cout << "iter used: " << iter->get_index_permutation() << " ( " << cur_interval.left() << " , " << cur_interval.right() << ")" ;
+                    //std::cout << "iter used: " << iter->get_index_permutation() << " ( " << cur_interval.left() << " , " << cur_interval.right() << ")" ;
                 }
-                std::cout << "" << std::endl;
+                //std::cout << "" << std::endl;
                 push_intersection(x_j, intersect_iter(wms,ranges));
             }
 
