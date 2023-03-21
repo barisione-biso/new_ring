47f036d78b679a23a20b1f1a0fa931b1e8a8c69f
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2023-03-20 21:12:27 -0300

    measures

---
+++ plots/boxplots_by_type_normal_measures_vs_best_gaos.py
@@ -0,0 +1,117 @@
+#Started as a copy of automated_test.py
+import os
+import csv
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import yticks
+import numpy as np
+
+import argparse
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+ 
+# Adding optional argument
+#parser.add_argument("-f", "--First", help = "First csv in the plot.", required=True)
+#parser.add_argument("-o", "--OutputFolder", help = "Output folder.")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+query_type_files = ["matplotlib/P2.txt",
+"matplotlib/P3.txt",
+"matplotlib/P4.txt",
+"matplotlib/T2.txt",
+"matplotlib/TI2.txt",
+"matplotlib/T3.txt",
+"matplotlib/TI3.txt",
+"matplotlib/J3.txt",
+"matplotlib/T4.txt",
+"matplotlib/TI4.txt",
+"matplotlib/J4.txt",
+"matplotlib/Tr1.txt",
+"matplotlib/Tr2.txt",
+"matplotlib/S1.txt",
+"matplotlib/S2.txt",
+"matplotlib/S3.txt",
+"matplotlib/S4.txt"]
+
+#query_types = ['J3', 'J4', 'P2', 'P3', 'P4', 'S1', 'S2', 'S3', 'S4', 'T2', 'T3', 'T4', 'TI2', 'TI3', 'TI4', 'Tr1', 'Tr2' ]
+query_types = ['P2', 'P3', 'P4', 'T2', 'TI2', 'T3', 'TI3', 'J3', 'T4', 'TI4', 'J4', 'Tr1', 'Tr2', 'S1', 'S2', 'S3', 'S4' ]
+
+labels_ = ['Ring', 'Ring Adaptive', 'Ring Muthu', 'Ring Adaptive Muthu', 'URing', 'URing Muthu', 'URing Adaptive', 'URing Adaptive Muthu']
+colors = ['lightgreen','brown', 'lightblue', 'black', 'red', 'blue', 'orange', 'purple', 'green', 'violet', 'olive',  'teal', 'cyan', 'lime', 'lightgray', 'darkgray', 'tan', 'pink', 'yellow']
+k = 0
+i = 0
+j = 0
+max_rows=3
+max_columns=6
+#first_bplot is declared here as a list but later replaced to store the 'artist' of the first boxplot.
+#The reason to do this is because the last bplot is deleted and therefore it cant be used as a valid handled for the legend.
+first_bplot = []
+print("****** Plotting Starts.")
+plt.rcParams['figure.figsize'] = [11.2, 8.05] #[12.8,9.6]
+fig, axes = plt.subplots(max_rows,max_columns) #2 rows with 4 columns
+while k < len(query_types):
+    ymax = 0.02
+    #hardcoded stuffs >>
+    if (i == 0 and j == max_columns - 1): #Omit plot 0,5 (Legend will be here)
+        j = 0
+        i = i + 1
+    if i == 1:
+        ymax = 0.015
+    elif i == 2:
+        ymax = 0.2
+    #hardcoded stuffs <<
+
+    print('Working with file '+query_type_files[k])
+    data = pd.read_csv(query_type_files[k]) #In Nanoseconds
+    data = data.div(1000000000, level=1, fill_value=0) #changed to seconds.
+    #It is important to reorder the dataframe since the CSV containing the measures is created with a map, therefore the order is not guaranted.
+    new_order = ['sigmod21', 'sigmod21_adaptive', 'one_ring_muthu_leap', 'one_ring_muthu_leap_adaptive', 'backward_only','backward_only_muthu', 'backward_only_adaptive', 'backward_only_adaptive_muthu']
+    data = data.reindex(columns=new_order)
+    bplot = axes[i, j].boxplot(
+        data,
+        patch_artist=True, # fill with color
+        showfliers=False
+    )
+    if not first_bplot:
+        first_bplot = bplot
+    #Setting the colors.
+    for patch, color in zip(bplot['boxes'], colors):
+        patch.set_facecolor(color)
+
+    #print ( 'i = ' + str(i) + ' , j = ' + str(j))
+    axes[i, j].set_ylim(0, ymax)
+    # yticks
+    #locs,labels = yticks()
+    #yticks(locs, map(lambda x: x, locs))
+    axes[i, j].set_title(query_types[k])
+    axes[i, j].get_xaxis().set_visible(False)
+    if j == 0:
+        axes[i, j].set_ylabel("Time (secs)")
+    else:
+       axes[i, j].set_yticklabels([])
+    j = j + 1
+    if j >= max_columns:
+        j = 0
+        i = i + 1
+    k = k + 1
+    #df.boxplot()
+
+fig.delaxes(axes[0,5]) #remove the last subplot of the first row (unused)
+#fig.suptitle("Tradeoff space-time")
+
+handles_ = [first_bplot["boxes"][0],first_bplot["boxes"][1],first_bplot["boxes"][2],first_bplot["boxes"][3],first_bplot["boxes"][4],first_bplot["boxes"][5],first_bplot["boxes"][6],first_bplot["boxes"][7]]
+fig.legend(handles = handles_, labels = labels_, loc="upper right", fontsize=8, ncol=1, bbox_to_anchor=(0.99, 0.9))
+fig.tight_layout() #Adds padding among subplots with enough size to show x & y labels.
+#Removing left padding.
+fig.subplots_adjust(
+    left=0.075,
+    hspace=0.2
+)
+plt.savefig('boxplots_by_type_ring_variants_only.pdf', format='pdf',dpi=500)
+plt.show()
+
+print("****** Plotting Ends.")
