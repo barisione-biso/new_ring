b5f38b1dd7f9937547c3154da6d028a77fb657a4
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2023-03-29 17:29:09 -0300

    Re-enabling ltj.join

--- src/query-index.cpp
+++ src/query-index.cpp
@@ -199,7 +199,7 @@ void query(const std::string &file, const std::string &queries, uint64_t number_
             if(ring::util::configuration.uses_reverse_index()){
                 if(ring::util::configuration.uses_leap()){
                     ring::ltj_algorithm_spo_sop_leap<ring_type,reverse_ring_type, wm_type> ltj(&query, &graph, &reverse_graph);
-                    //ltj.join(res, number_of_results, timeout_in_millis);
+                    ltj.join(res, number_of_results, timeout_in_millis);
 
                     stop = high_resolution_clock::now();
                     time_span = duration_cast<microseconds>(stop - start);
@@ -213,7 +213,7 @@ void query(const std::string &file, const std::string &queries, uint64_t number_
                     cout << nQ <<  ";" << res.size() << ";" << (unsigned long long)(total_time*1000000000ULL) << ";"<< ltj.get_gao(ht) << endl;
                 }else{
                     ring::ltj_algorithm_spo_sop<ring_type,reverse_ring_type, wm_type> ltj(&query, &graph, &reverse_graph);
-                    //ltj.join(res, number_of_results, timeout_in_millis);
+                    ltj.join(res, number_of_results, timeout_in_millis);
 
                     stop = high_resolution_clock::now();
                     time_span = duration_cast<microseconds>(stop - start);
@@ -229,7 +229,7 @@ void query(const std::string &file, const std::string &queries, uint64_t number_
             }
             else{
                 ring::ltj_algorithm<ring_type> ltj(&query, &graph);
-                //ltj.join(res, number_of_results, timeout_in_millis);
+                ltj.join(res, number_of_results, timeout_in_millis);
 
                 stop = high_resolution_clock::now();
                 time_span = duration_cast<microseconds>(stop - start);
